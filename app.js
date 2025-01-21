0b68f7c46a7b43e65f73c91508e1ec6f6941b280
Carlos Vivanco <carlos.vivanco.08@outlook.com>
2025-01-21 11:59:35 -0300

    first commit

--- app.js
+++ app.js
@@ -2,29 +2,26 @@ const pronoun = ["the", "our"];
 const adj = ["great", "big"];
 const noun = ["jogger", "racoon"];
 
-// Función para generar todas las combinaciones
 function generateAllUrls() {
-  const urls = []; // Aquí almacenaremos las combinaciones
+  const urls = [];
 
-  // Tres bucles anidados para recorrer los arreglos
   for (let p of pronoun) {
     for (let a of adj) {
       for (let n of noun) {
-        urls.push(`www.${p}_${a}_${n}.com`); // Crear y guardar cada URL
+        urls.push(`www.${p}_${a}_${n}.com`);
       }
     }
   }
 
-  return urls; // Devolver el arreglo con todas las combinaciones
+  return urls;
 }
 
 window.onload = function () {
-  const urls = generateAllUrls(); // Generar todas las URLs
+  const urls = generateAllUrls();
   const container = document.querySelector("#excuse");
 
-  // Crear un elemento <p> por cada URL y agregarlo al contenedor
   urls.forEach((url) => {
-    const p = document.createElement("p");
+    const p = document.createElement("h1");
     p.textContent = url;
     container.appendChild(p);
   });
